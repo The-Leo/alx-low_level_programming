07697827c0ddeb8237821f031e2b592fbb46c912
Leonard Odoi <odoi.leonard1@gmail.com>
2022-12-08 12:28:51 -0800

    Write a C program that prints a sting with the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - print a string using puts
+ *
+ * Return: always 0
+*/
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
