0bf3350353012afab50b4d01352ca8c31ec7e8b7
Leonard Odoi <odoi.leonard1@gmail.com>
2022-12-08 12:42:02 -0800

    Fix Betty errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/*
- * main - print a string using puts
+/**
+ * main - print a line of code using puts
  *
  * Return: always 0
 */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
