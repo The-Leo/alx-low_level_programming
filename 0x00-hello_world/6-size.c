63101549294055c454f7323a0e956a57a5b84f30
Leonard Odoi <odoi.leonard1@gmail.com>
2022-12-08 21:45:39 -0800

    Modify C program that prints the size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,12 +10,12 @@ int main(void)
 	int b;
 	long int c;
 	long long int d;
-	float flt;
+	float f;
 
 	printf("size of a char: %lu byte(S)\n", (unsigned long)sizeof(a));
 	printf("size of an int: %lu byte(S)\n", (unsigned long)sizeof(b));
 	printf("size of a long int: %lu byte(S)\n", (unsigned long)sizeof(c));
 	printf("size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
-	printf("size of a flt: %lu byte(S)\n", (unsigned long)sizeof(flt));
+	printf("size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
 	return (0);
 }
