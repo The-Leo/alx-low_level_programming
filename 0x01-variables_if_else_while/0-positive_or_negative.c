11b4542b28240db34de333a581fc15cc67910d2e
Leonard Odoi <odoi.leonard1@gmail.com>
2022-12-10 14:52:34 -0800

    Assign a random number to the variable n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - print a random number
+ *
+ * Return: 0
+*/
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
