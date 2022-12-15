ea84756fe420c5fb1f13dcb2e4f483a81e8a64cb
Leonard Odoi <odoi.leonard1@gmail.com>
2022-12-14 21:14:06 -0800

    Edit comments

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * main - prints _putchar and a new line
- * Return: Always 0
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
