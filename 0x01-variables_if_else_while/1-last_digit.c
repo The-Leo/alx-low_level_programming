f1a786d16a9bfc949cfc335bbd75359bed6eb3e1
Leonard Odoi <odoi.leonard1@gmail.com>
2022-12-21 13:20:27 -0800

    Write a program that assigns a random number to n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,31 +3,34 @@
 #include <stdio.h>
 
 /**
- * main - Entry
+ * main - Prints the last digit of a randomly generated number
+ *        and whether it is greater than 5, less than 6, or 0.
  *
- * Return: 0
-*/
-
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int n;
-	int x;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	x = n % 10
-	printf("Last digit of %d is %d, n, x);
-	if (x > 5)
+
+	if ((n % 10) > 5)
 	{
-		printf("and is greater than 5");
+		printf("Last digit of %d is %d and is greater than 5\n",
+			n, n % 10);
 	}
-	if (x==0)
+	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
-		printf("and is 0");
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+			n, n % 10);
 	}
-	if (x < 6 && x !=0)
-		printf("and is less than 6 and not 0");
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n",
+			n, n % 10);
 	}
-	printf("\n");
+
 	return (0);
+
 }
