22f1208f02be302b7167e6a2ef24999ee301fe73
Leonard Odoi <odoi.leonard1@gmail.com>
2022-12-22 15:50:22 -0800

    Reduce line to less than 80 chars

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,9 +5,9 @@
  * @s1: A pointer to the first string to be compared.
  * @s2: A pointer to the second string to be compared.
  *
- * Return: If str1 < str2, the negative difference of the first unmatched characters.
+ * Return: If str1 < str2, the negative difference of the first unmatched char
  *         If str1 == str2, 0.
- *         If str1 > str2, the positive difference of the first unmatched characters.
+ *         If str1 > str2, the positive difference of the first unmatched char
  */
 int _strcmp(char *s1, char *s2)
 {
