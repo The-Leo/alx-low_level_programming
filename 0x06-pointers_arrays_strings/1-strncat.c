c00e32bb7bedbe0cab5e2746017fedae0d76dc7a
Leonard Odoi <odoi.leonard1@gmail.com>
2022-12-22 15:38:47 -0800

    Write a function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
