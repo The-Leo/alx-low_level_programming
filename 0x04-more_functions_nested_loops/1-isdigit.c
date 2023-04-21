5d33470e130af67b79baa7c95121b4f52f20f9d9
Leonard Odoi <odoi.leonard1@gmail.com>
2023-04-21 15:56:12 -0700

    Code from gpt test

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * _isdigit - Checks for a digit (0-9).
- * @c: The number to be checked.
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: the character to be checked
  *
- * Return: 1 if the number is a digit, 0 otherwise.
+ * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-
-	else
-		return (0);
+    if (c >= '0' && c <= '9')
+    {
+        return (1); /* c is a digit */
+    }
+    else
+    {
+        return (0); /* c is not a digit */
+    }
 }
