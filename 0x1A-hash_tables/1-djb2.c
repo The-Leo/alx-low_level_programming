8033ff69de825623d10c50285aebe3c1f2e866c0
Leonard Odoi <odoi.leonard1@gmail.com>
2023-07-12 16:36:49 -0700

    Update djb2 algorithm

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -6,15 +6,14 @@
  *
  * Return: The calculated hash.
  */
-
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
-	int count;
+	int c;
 
 	hash = 5381;
-	while ((count = *str++))
-		hash = ((hash << 5) + hash) + count; /* hash * 33 + count */
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
 	return (hash);
 }
