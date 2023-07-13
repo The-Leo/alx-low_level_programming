efc1760f02a4b35befe88553f3720967f899b963
Leonard Odoi <odoi.leonard1@gmail.com>
2023-07-12 17:17:49 -0700

    Include header files in hash table.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,10 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  * @key: The key, string
@@ -35,6 +39,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+
 /**
  * struct shash_node_s - Node of a sorted hash table
  * @key: The key, string
@@ -77,4 +82,4 @@ void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
 
-#endif
+#endif /* HASH_TABLES_H */
